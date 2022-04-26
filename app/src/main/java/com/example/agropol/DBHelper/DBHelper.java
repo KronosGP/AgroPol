2f710a118265ba500eb0c1581c302cc3fd4931e7
KronosGP <57954795+KronosGP@users.noreply.github.com>
2022-04-26 20:06:16 +0200

    Ukończona baza danych

--- app/src/main/java/com/example/agropol/DBHelper/DBHelper.java
+++ app/src/main/java/com/example/agropol/DBHelper/DBHelper.java
@@ -30,12 +30,34 @@ public class DBHelper extends SQLiteOpenHelper {
                 "`Nazwisko` VARCHAR(30) NOT NULL , " +
                 "`Adres_Email` TEXT NOT NULL , " +
                 "`Tel` INT(9) NOT NULL)");
-        exec(sqLiteDatabase,"CREATE TABLE `agropol`.`sadzonki` ( `ID` INT NOT NULL AUTO_INCREMENT , `Gatunek` VARCHAR(30) NOT NULL , `Odmiana` VARCHAR(30) NOT NULL , `Ilosc` INT NOT NULL , `Cena` FLOAT NOT NULL , PRIMARY KEY (`ID`))");
+
+        sqLiteDatabase.execSQL("CREATE TABLE `agropol`.`sadzonki` " +
+                "( `ID` INT NOT NULL PRIMARY KEY AUTOINCREMENT , " +
+                "`Gatunek` VARCHAR(30) NOT NULL , " +
+                "`Odmiana` VARCHAR(30) NOT NULL , " +
+                "`Ilosc` INT NOT NULL , " +
+                "`Cena` FLOAT NOT NULL )");
+
+        sqLiteDatabase.execSQL("CREATE TABLE `agropol`.`zamowienie` " +
+                "( `ID` INT NOT NULL  PRIMARY KEY AUTOINCREMENT , " +
+                "`IDKlienta` INT NOT NULL , " +
+                "`Cena` FLOAT NOT NULL , " +
+                "`Data_zamowienia` DATE NOT NULL  )");
+
+        sqLiteDatabase.execSQL("CREATE TABLE `agropol`.`szczegoly_zamowienia` " +
+                "( `IDZamowienia` INT NOT NULL , " +
+                "`IDSadzonki` INT NOT NULL , " +
+                "`Ilość` INT NOT NULL )");
+
+        sqLiteDatabase.execSQL("CREATE TABLE `agropol`.`reklamacja` " +
+                "( `ID` INT NOT NULL PRIMARY KEY AUTOINCREMENT , " +
+                "`IDKlienta` INT NOT NULL , " +
+                "`IDZamowienia` INT NOT NULL , " +
+                "`IDPracownika` INT , " +
+                "`Tresc` TEXT NOT NULL , " +
+                "`Stan` TEXT NOT NULL )");
     }
 
-    private void exec(SQLiteDatabase sqLiteDatabase, String s) {
-        sqLiteDatabase.execSQL(s);
-    }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
